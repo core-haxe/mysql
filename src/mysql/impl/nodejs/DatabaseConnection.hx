a9dde45e2ee43ed3c46f52d143bdbbc5f67d53fc
Ian Harrigan <ianharrigan@hotmail.com>
2024-01-03 22:35:54 +0100

    add logging

--- src/mysql/impl/nodejs/DatabaseConnection.hx
+++ src/mysql/impl/nodejs/DatabaseConnection.hx
@@ -4,12 +4,22 @@ import promises.Promise;
 import mysql.externs.nodejs.MySql2;
 import js.node.Buffer;
 import mysql.externs.nodejs.Connection as NativeConnection;
+import logging.Logger;
 
 class DatabaseConnection extends DatabaseConnectionBase {
+    private static var log = new Logger(DatabaseConnection, true);
+
     private var _nativeConnection:NativeConnection = null;
 
     public override function open():Promise<MySqlResult<Bool>> {
         return new Promise((resolve, reject) -> {
+            log.debug("creating connection:", {
+                host: this.connectionDetails.host,
+                user: this.connectionDetails.user,
+                password: this.connectionDetails.pass,
+                database: this.connectionDetails.database,
+                rowsAsArray: false
+            });
             _nativeConnection = MySql2.createConnection({
                 host: this.connectionDetails.host,
                 user: this.connectionDetails.user,
@@ -31,6 +41,7 @@ class DatabaseConnection extends DatabaseConnectionBase {
     
     public override function exec(sql:String):Promise<MySqlResult<Bool>> {
         return new Promise((resolve, reject) -> {
+            log.data("exec:", sql);
             _nativeConnection.execute(sql, (error, rows, fields) -> {
                 if (error != null) {
                     if (!checkForDisconnection(error.message, CALL_EXEC, sql, null, resolve, reject)) {
@@ -46,6 +57,7 @@ class DatabaseConnection extends DatabaseConnectionBase {
 
     public override function get(sql:String, ?param:Dynamic):Promise<MySqlResult<Dynamic>> {
         return new Promise((resolve, reject) -> {
+            log.data("get:", [sql, param]);
             _nativeConnection.execute(sql, params(param), (error, rows, fields) -> {
                 if (error != null) {
                     if (!checkForDisconnection(error.message, CALL_GET, sql, param, resolve, reject)) {
@@ -72,6 +84,7 @@ class DatabaseConnection extends DatabaseConnectionBase {
 
     public override function query(sql:String, ?param:Dynamic):Promise<MySqlResult<Array<Dynamic>>> {
         return new Promise((resolve, reject) -> {
+            log.data("query:", [sql, param]);
             _nativeConnection.query(sql, params(param), (error, rows, fields) -> {
                 if (error != null) {
                     if (!checkForDisconnection(error.message, CALL_QUERY, sql, param, resolve, reject)) {
@@ -88,6 +101,7 @@ class DatabaseConnection extends DatabaseConnectionBase {
 
     public override function all(sql:String, ?param:Dynamic):Promise<MySqlResult<Array<Dynamic>>> {
         return new Promise((resolve, reject) -> {
+            log.data("all:", [sql, param]);
             _nativeConnection.execute(sql, params(param), (error, rows, fields) -> {
                 if (error != null) {
                     if (!checkForDisconnection(error.message, CALL_ALL, sql, param, resolve, reject)) {
